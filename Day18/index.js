483594ad2f7cd16584f68a149668b52260c014c2
Martin Wildeman <mhwildeman@gmail.com>
2020-12-19 12:22:19 +0100

    Day 18, part 2

--- Day18/index.js
+++ Day18/index.js
@@ -9,49 +9,59 @@ const list = fs.readFileSync('input.txt', { encoding: 'utf8' });
 let inputArray = parse(list, { delimiter: ';' }).map(a => a[0]);
 
 const solveSum = function (sum) {
-    //console.log(sum);
     let result = 0;
     sum = sum.replace(/\s/g, '');
     let matches = sum.match(/\([^\(^\)]*\)/g);
     while (matches) {
         for (var i = 0; i < matches.length; i++) {
             let subResult = solveSum(matches[i].slice(1, -1));
-            sum = sum.replace(matches[i],subResult.toString());
+            sum = sum.replace(matches[i], subResult.toString()); 
         }
         matches = sum.match(/\([^\(^\)]*\)/g);
     }
-    
-        let sumArray = sum.split('');
-        let lastOperator = '+';
-        let stringRep = '';
-        sumArray.forEach(element => {
-            switch (element) {
-                case '+':
-                case '*':
-                    switch (lastOperator) {
-                        case '+':
-                            result += +stringRep;
-                            break;
-                        case '*':
-                            result *= +stringRep;
-                            break;
-                    }
-                    stringRep = '';
-                    lastOperator = element;
-                    break;
-                default:
-                    stringRep += element;
-
+    matches = sum.match(/\d+\+\d+/g);
+    if(matches && matches.length>=1 && matches[0]!==sum)
+    {
+        while (matches) {
+            for (var i = 0; i < matches.length; i++) {
+                let subResult = solveSum(matches[i]);
+                sum = sum.replace(matches[i], subResult.toString());
             }
-        });
-        switch (lastOperator) {
-            case '+':
-                result += +stringRep;
-                break;
-            case '*':
-                result *= +stringRep;
-                break;
+            matches = sum.match(/\d+\+\d+/g);
         }
+    }
+
+    let sumArray = sum.split('');
+    let lastOperator = '+';
+    let stringRep = '';
+    sumArray.forEach(element => {
+        switch (element) {
+            case '+':
+            case '*':
+                switch (lastOperator) {
+                    case '+':
+                        result += +stringRep;
+                        break;
+                    case '*':
+                        result *= +stringRep;
+                        break;
+                }
+                stringRep = '';
+                lastOperator = element;
+                break;
+            default:
+                stringRep += element;
+
+        }
+    });
+    switch (lastOperator) {
+        case '+':
+            result += +stringRep;
+            break;
+        case '*':
+            result *= +stringRep;
+            break;
+    }
     return result;
 }
 
